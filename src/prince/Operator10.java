6c9463f6651f3469f815271a4ba7f66c8dd4e7a3
Vishal <Vishal@LAPTOP-GHGBDL9L>
2022-01-20 19:50:35 +0530

    First Commit

---
+++ src/prince/Operator10.java
@@ -0,0 +1,13 @@
+package prince;
+
+public class Operator10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=70;
+		System.out.println(a+b);
+
+	}
+
+}
